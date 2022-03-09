d48760d8f75d1718f787bc0194f2555b33484a6a
Jakub Białoskórski <bialoskorski@icloud.com>
2022-03-09 19:48:32 +0100

    add bitcoin price checker

---
+++ bitcoin.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+import requests
+import ZeroSeg.led as led
+
+device = led.sevensegment(cascaded=2)
+device.clear()
+
+level = 1
+device.brightness(level)
+
+response = requests.get('https://api.coindesk.com/v1/bpi/currentprice.json')
+data = response.json()
+device.write_text(1, data["bpi"]["USD"]["rate"][:8])
